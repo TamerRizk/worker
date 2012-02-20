3c92ead480bef0550b727d42eff9f9878439e0c5
Tamer Rizk <trizk@inficron.com>
2012-02-20 07:12:13 +0200

    Update example/worker.js

--- example/worker.js
+++ example/worker.js
@@ -1,1 +1,17 @@
-function onmessage(s){s='Child got: '+s;workermessage(s);}var i=1000000;var m=0;var o=[];while(i>0){i=i-1;m=Math.floor(Math.ceil(Math.random()*1000000)/(Math.ceil(Math.random()*1000)+1));if(m%19==0){o.push(m);}}workermessage(o.join(', '));workercompleted('done!');
+hasWorker = function() {
+  //return false;
+  return (typeof(Worker) != "undefined") ? true : false;
+};
+
+(function(){
+  var i = 1000000;
+  var x = 0;
+  var y = 0;
+  while(--i>0){
+     x = Math.floor(Math.ceil(Math.random()*1000000)/(Math.ceil(Math.random()*1000)+1));
+     if(x%19==0)
+        y+=x;
+  }
+  hasWorker ? postMessage(y) : workermessage(y);
+  arguments.callee();
+})();
